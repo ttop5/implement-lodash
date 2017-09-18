17dd23849632dd644a71c27e126668a7c21039e0
ttop5 <ttop5@qq.com>
2017-09-18 16:03:59 +0800

    feat(Array/_.reverse.js): _.reverse

---
+++ Array/_.reverse.js
@@ -0,0 +1,19 @@
+/**
+* 反转array，使得第一个元素变为最后一个元素，第二个元素变为倒数第二个元素，依次类推。
+* Note: 这个方法会改变原数组 array，基于 Array#reverse。
+*
+* @param  {Array} array 要修改的数组
+* @return {Array}       返回 array
+* @example
+*
+* var array = [1, 2, 3];
+*
+* reverse(array);
+* // => [3, 2, 1]
+*
+* console.log(array);
+* // => [3, 2, 1]
+*/
+function reverse(array) {
+  return array.reverse();
+}
