ee59844d1b9633ac819b393ff7cf3d606d9c627c
ttop5 <ttop5@qq.com>
2017-09-18 16:53:41 +0800

    feat(Array/_.sortedUniq.js): _.sortedUniq

---
+++ Array/_.sortedUniq.js
@@ -0,0 +1,13 @@
+/**
+* 这个方法类似 _.uniq，除了它会优化排序数组。
+*
+* @param  {Array} array 要检查的数组
+* @return {Array}       返回一个不重复的排序数组
+* @example
+*
+* sortedUniq([1, 1, 2]);
+* // => [1, 2]
+*/
+function sortedUniq(array) {
+  return [...new Set(array.sort())];
+}
