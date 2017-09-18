182904ccce716d14e200882d18ac5f3e01d717d1
ttop5 <ttop5@qq.com>
2017-09-18 15:08:52 +0800

    feat(Array/_.last.js): _.last

---
+++ Array/_.last.js
@@ -0,0 +1,13 @@
+/**
+* 获取array中的最后一个元素。
+*
+* @param  {Array}  array 要检索的数组
+* @return {Number}       返回array中的最后一个元素
+* @example
+*
+* last([1, 2, 3]);
+* // => 3
+*/
+function last(array) {
+  return array[array.length-1];
+}
