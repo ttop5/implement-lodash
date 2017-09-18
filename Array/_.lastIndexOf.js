96a40483563dc6853836306c1fdbf25afab1bfab
ttop5 <ttop5@qq.com>
2017-09-18 15:19:39 +0800

    feat(Array/_.lastIndexOf.js): _.lastIndexOf

---
+++ Array/_.lastIndexOf.js
@@ -0,0 +1,19 @@
+/**
+* 这个方法类似 _.indexOf ，区别是它是从右到左遍历array的元素。
+*
+* @param  {Array}  array                      要搜索的数组
+* @param  {NUmber} value                      要搜索的值
+* @param  {Number} [fromIndex=array.length-1] 开始搜索的索引值
+* @return {Number}                            返回匹配值的索引值，否则返回 -1
+* @example
+*
+* lastIndexOf([1, 2, 1, 2], 2);
+* // => 3
+*
+* // Search from the `fromIndex`.
+* lastIndexOf([1, 2, 1, 2], 2, 2);
+* // => 1
+*/
+function lastIndexOf(array, value, fromIndex=array.length-1) {
+  return array.lastIndexOf(value, fromIndex);
+}
