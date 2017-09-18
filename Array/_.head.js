b358eed8f52b70b55f32fe93fe4e03f3bd367f98
ttop5 <ttop5@qq.com>
2017-09-18 14:20:51 +0800

    feat(Array/_.head.js): _.head

---
+++ Array/_.head.js
@@ -0,0 +1,16 @@
+/***
+* 获取数组 array 的第一个元素。
+*
+* @param  {Array} array [description]
+* @return {Number}      [description]
+* @example
+*
+* head([1, 2, 3]);
+* // => 1
+*
+* head([]);
+* // => undefined
+*/
+function head(array) {
+  return array[0];
+}
