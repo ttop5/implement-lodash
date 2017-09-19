bd12fe07cb38fe8ddd41d1eceb28e7a29d20a1c6
ttop5 <ttop5@qq.com>
2017-09-19 17:05:14 +0800

    feat(Math/_.divide.js): _.divide

---
+++ Math/_.divide.js
@@ -0,0 +1,14 @@
+/**
+ * 两个数相除。
+ *
+ * @param  {Number} dividend 被除数
+ * @param  {Number} divisor  除数
+ * @return {Number}          返回商数
+ * @example
+ *
+ * divide(6, 4);
+ * // => 1.5
+ */
+function divide(dividend, divisor) {
+  return dividend/divisor;
+}
