500923f8487c9d0733a547bee22c9fecfe55d0e4
ttop5 <ttop5@qq.com>
2017-09-19 16:38:18 +0800

    feat(Math/_.add.js): _.add

---
+++ Math/_.add.js
@@ -0,0 +1,14 @@
+/**
+* 两个数相加。
+*
+* @param  {Number} augend 被加数
+* @param  {Number} addend 加数
+* @return {Number}        返回总和
+* @example
+*
+* add(6, 4);
+* // => 10
+*/
+function add(augend, addend) {
+  return (augend + addend);
+}
