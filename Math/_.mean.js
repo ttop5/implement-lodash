10e6e38a8724a9c30947636ce3f4d9de5fbc85cb
ttop5 <ttop5@qq.com>
2017-09-20 10:03:47 +0800

    feat(Math/_.mean.js): _.mean

---
+++ Math/_.mean.js
@@ -0,0 +1,13 @@
+/**
+*计算 array 的平均值。
+*
+* @param  {Array}  array 要迭代的数组
+* @return {Number}       返回平均值
+* @example
+*
+* mean([4, 2, 8, 6]);
+* // => 5
+*/
+function mean(array) {
+  return eval(array.join('+'))/array.length;
+}
