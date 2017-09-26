6d24ad2d8006a964ed138dc6cf9afb2fa566303b
ttop5 <ttop5@qq.com>
2017-09-26 15:27:04 +0800

    feat(Math/_.subtract.js): _.subtract

---
+++ Math/_.subtract.js
@@ -0,0 +1,14 @@
+/**
+* 两数相减。
+*
+* @param  {Number} minuend    被减数
+* @param  {Number} subtrahend 减数
+* @return {Number}            返回差
+* @example
+*
+* subtract(6, 4);
+* // => 2
+*/
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
