5e783cf50af109bf632224f2d3e055b8c59b8c2a
ttop5 <ttop5@qq.com>
2017-10-16 19:07:05 +0800

    feat(Lang/_.isError.js): _.isError

---
+++ Lang/_.isError.js
@@ -0,0 +1,16 @@
+/**
+* 检查 value 是否是 Error, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, 或者 URIError对象。
+*
+* @param  {*}  value  待检查的值
+* @return {Boolean}   如果 value 是一个错误（Error）对象，那么返回 true，否则返回 false
+* @example
+*
+* isError(new Error);
+* // => true
+*
+* isError(Error);
+* // => false
+*/
+function isError(value) {
+  return (value instanceof Error);
+}
