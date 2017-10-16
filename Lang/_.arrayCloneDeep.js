b1cf251bcdf13cf8a7da16c43eda18a6eab1b054
ttop5 <ttop5@qq.com>
2017-10-16 16:26:55 +0800

    feat(Lang/_.arrayCloneDeep.js): _.arrayCloneDeep

--- Lang/_.arrayCloneDeep.js
+++ Lang/_.arrayCloneDeep.js
@@ -1,15 +1,15 @@
 /**
-* 创建一个数组的浅拷贝。
+* 创建一个数组的深拷贝。
 *
 * @param  {Array} array 要拷贝的数组
 * @return {Array}       返回拷贝后的数组
 * @example
 *
 * var array1 = [1, 2, 3];
-* var array2 = arrayClone(array1);
+* var array2 = arrayCloneDeep(array1);
 * console.log(array2 === array1);
-* // => true
+* // => false
 */
-function arrayClone(array) {
-  return array;
+function arrayCloneDeep(array) {
+  return [...array];
 }
