a261338540dcb4b11c10b99a1275c9aab2482be3
ttop5 <ttop5@qq.com>
2017-10-16 16:22:15 +0800

    feat(Lang/_.arrayClone.js): _.arrayClone

---
+++ Lang/_.arrayClone.js
@@ -0,0 +1,15 @@
+/**
+* 创建一个数组的浅拷贝。
+*
+* @param  {Array} array 要拷贝的数组
+* @return {Array}       返回拷贝后的数组
+* @example
+*
+* const array1 = [1, 2, 3];
+* const array2 = arrayClone(array1);
+* console.log(array2 === array1);
+* // => true
+*/
+function arrayClone(array) {
+  return array;
+}
