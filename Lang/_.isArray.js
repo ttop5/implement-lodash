f27c32826b28817ec645a558fa5778f4435e7053
ttop5 <ttop5@qq.com>
2017-10-16 16:58:08 +0800

    feat(Lang/_.isArray.js): _.isArray

---
+++ Lang/_.isArray.js
@@ -0,0 +1,22 @@
+/**
+* 检查 value 是否是 Array 类对象。
+*
+* @param  {*}  value  要检查的值
+* @return {Boolean}   如果value是一个数组返回 true，否则返回 false
+* @example
+*
+* isArray([1, 2, 3]);
+* // => true
+*
+* isArray(document.body.children);
+* // => false
+*
+* isArray('abc');
+* // => false
+*
+* isArray(_.noop);
+* // => false
+*/
+function isArray(value) {
+  return Array.isArray(value);
+}
