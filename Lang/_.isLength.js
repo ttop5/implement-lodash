8634850bd8ee317e53d5ce7e8ab5cc32e0759eab
ttop5 <ttop5@qq.com>
2017-11-13 22:43:22 +0800

    feat(Lang/_.isLength.js): _.isLength

---
+++ Lang/_.isLength.js
@@ -0,0 +1,25 @@
+/**
+* 检查 value 是否为有效的类数组长度。
+*
+* @param  {*}  value  要检查的值
+* @return {Boolean}   如果 value 是一个有效长度，那么返回 true，否则返回 false
+* @example
+*
+* isLength(3);
+* // => true
+*
+* isLength(Number.MIN_VALUE);
+* // => false
+*
+* isLength(Infinity);
+* // => false
+*
+* isLength('3');
+* // => false
+*/
+function isLength(value) {
+  return typeof value === 'number'
+  && value > -1
+  && value % 1 === 0
+  && value <= Number.MAX_SAFE_INTEGER;
+}
