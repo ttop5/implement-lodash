7cbee7146cfd581431af78157b00b26762cbc2ba
ttop5 <ttop5@qq.com>
2017-12-25 16:00:55 +0800

    feat(String/_.lowerFirst.js): _.lowerFirst

---
+++ String/_.lowerFirst.js
@@ -0,0 +1,17 @@
+/**
+ * 转换字符串string的首字母为小写。
+ *
+ * @param   {string} [string=''] 要转换的字符串
+ * @returns {string}             返回转换后的字符串
+ * @example
+ *
+ * lowerFirst('Fred');
+ * // => 'fred'
+ *
+ * lowerFirst('FRED');
+ * // => 'fRED'
+ */
+
+function lowerFirst(string) {
+  return string.replace(string[0], string[0].toLowerCase());
+}
