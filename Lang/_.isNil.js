0adbd8a3db47c1aeb8ac0bf588ee6a4d45b45b6a
ttop5 <ttop5@qq.com>
2017-12-25 14:27:12 +0800

    feat(Lang/_.isNil.js): _.isNil

---
+++ Lang/_.isNil.js
@@ -0,0 +1,20 @@
+/**
+ * 检查 value 是否是 null 或者 undefined。
+ *
+ * @param   {*}  value 要检查的值
+ * @returns {boolean}  如果 value 为null 或 undefined，那么返回 true，否则返回 false
+ * @example
+ *
+ * isNil(null);
+ * // => true
+ *
+ * isNil(void 0);
+ * // => true
+ *
+ * isNil(NaN);
+ * // => false
+ */
+
+function isNil(value) {
+  return value === null || value === undefined;
+}
