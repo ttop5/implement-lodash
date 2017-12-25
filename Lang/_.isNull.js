35062264c321785815e155d8e95958e49d425e00
ttop5 <ttop5@qq.com>
2017-12-25 14:30:54 +0800

    feat(Lang/_.isNull.js): _.isNull

---
+++ Lang/_.isNull.js
@@ -0,0 +1,17 @@
+/**
+ * 检查 valuealue 是否是 null。
+ *
+ * @param   {*}  value 要检查的值
+ * @returns {boolean}  如果 value 为null，那么返回 true，否则返回 false
+ * @example
+ *
+ * isNull(null);
+ * // => true
+ *
+ * isNull(void 0);
+ * // => false
+ */
+
+function isNull(value) {
+  return value === null;
+}
