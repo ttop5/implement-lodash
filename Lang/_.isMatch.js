d307e9b94e4355f7d6d86c4fd7b9f4e492a70c85
ttop5 <ttop5@qq.com>
2017-12-25 11:47:50 +0800

    feat(Lang/_.isMatch.js): _.isMatch

---
+++ Lang/_.isMatch.js
@@ -0,0 +1,28 @@
+/**
+ * 执行一个深度比较，来确定 object 是否含有和 source 完全相等的属性值。
+ *
+ * @param   {Object}  要检查的对象
+ * @param   {Object}  属性值匹配的对象
+ * @returns {boolean} 如果object匹配，那么返回 true，否则返回 false
+ * @example
+ *
+ * var object = { 'a': 1, 'b': 2 };
+ *
+ * isMatch(object, { 'b': 2 });
+ * // => true
+ *
+ * isMatch(object, { 'b': 1 });
+ * // => false
+ */
+
+ function isMatch(object, source) {
+   const [oKeys, oValues] = [Object.keys(object), Object.values(object)];
+   const [sKey, sValue] = [Object.keys(source)[0], Object.values(source)[0]];
+   for (let i = 0; i < oKeys.length; i++) {
+     console.log(oKeys[i], oValues[i])
+     if (oKeys[i] === sKey && oValues[i] === sValue) {
+       return true;
+     }
+   }
+   return false;
+ }
