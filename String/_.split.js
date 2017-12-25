f18c410fb9809cd6c612a37fc2da85da6983ea7e
ttop5 <ttop5@qq.com>
2017-12-25 17:33:08 +0800

    feat(String/_.split.js): _.split

---
+++ String/_.split.js
@@ -0,0 +1,16 @@
+/**
+ * 根据separator 拆分字符串string。
+ *
+ * @param   {string}        [string=''] 要拆分的字符串
+ * @param   {RegExp|string} separator   拆分的分隔符
+ * @param   {number}        [limit]     限制结果的数量
+ * @returns {Array}
+ * @example
+ *
+ * _.split('a-b-c', '-', 2);
+ * // => ['a', 'b']
+ */
+
+function split(string, separator, limit) {
+  return string.split(separator, limit);
+}
