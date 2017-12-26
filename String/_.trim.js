67ac164620ed1d7bb1adad5784755286388a8915
ttop5 <ttop5@qq.com>
2017-12-26 11:47:31 +0800

    feat(String/_.trim.js): _.trim

---
+++ String/_.trim.js
@@ -0,0 +1,14 @@
+/**
+ * 从string字符串中移除前面和后面的空格。
+ *
+ * @param   {string} [string=''] 要处理的字符串
+ * @returns {string}             返回处理后的字符串
+ * @example
+ *
+ * trim('  abc  ');
+ * // => 'abc'
+ */
+
+ function trim(string) {
+   return string.trim();
+ }
